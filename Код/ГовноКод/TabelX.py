1844d202fef68c4d4b6f2dd14bd67c255b7a81cc
Yuri <114381954+Prostochell-228@users.noreply.github.com>
2022-10-22 10:49:24 +0700

    Update TabelX.py

--- Код/ГовноКод/TabelX.py
+++ Код/ГовноКод/TabelX.py
@@ -1,6 +1,20 @@
+
 p=int(input())
 for i in  range(1, p+1):
     for g in range(1, p+1):
         x=(i*g//10)*10+(i*g)%10
         print(x, end=' ')
     print()
+   
+
+
+print("Основание системы")
+p = int(input())
+X = 1
+Y = 1
+for X in range(X,p):
+	for Y in range(Y,p):
+		Z = (X*Y //p)*10 + (X*Y)%p
+		print(Z, end=" ")
+	print()
+	Y = 1
